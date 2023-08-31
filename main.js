ddf81ea810a748d84052779b1165b29c67669cb2
Mateus Bragatto <mateusbragatto@estudante.ufscar.br>
2023-08-30 23:17:41 -0300

    Adding cameras

--- main.js
+++ main.js
@@ -12,6 +12,21 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 // Linkando renderizador com o html
 document.body.appendChild( renderer.domElement ); // codigo javascript main
 
+
+// Definição das cameras e suas posições
+camera_superior.position.set(0, 25, 0); // Posiciona a câmera acima da cena
+camera_superior.lookAt(new THREE.Vector3(0, 0, 0)); // Aponta a câmera para o centro da cena
+camera_superior.up.set(0, 0, -1); // Define o vetor "up" da câmera para apontar para baixo
+camera_superior.updateMatrixWorld();
+
+camera_diagonal.position.set(5, 6, 10);
+camera_diagonal.lookAt(scene.position);
+camera_diagonal.updateMatrixWorld();
+
+camera_frontal.position.set(0, 0, 15);
+camera_frontal.lookAt(scene.position);
+camera_frontal.updateMatrixWorld();
+
 // Loop de renderização => Animar a cena
 function animate() {
 	requestAnimationFrame( animate );
